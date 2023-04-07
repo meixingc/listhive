993c840a1adbae5fa7049399171936cff1cc2aa0
meixingc <realmeixing@gmail.com>
2023-04-06 23:30:59 -0400

    initial models + settings auth user model change

--- listhive_project/listhive/models.py
+++ listhive_project/listhive/models.py
@@ -1,10 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
-class Todo(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
+# User Models
+class CustomUser(AbstractUser):
+    photo = models.TextField(default='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png')
+    def get_followers(self):
+        return self.followers.all()
+    def get_following(self):
+        return self.following.all()
 
-    def _str_(self):
-        return self.title
+class Follower(models.Model):
+    follower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='following')
+    followee = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='followers')
+    class Meta:
+        unique_together = ('follower', 'followee')
+
+class Favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='favorites')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    class Meta:
+        unique_together = ('user', 'content_type', 'object_id')
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    class Meta:
+        unique_together = ('user', 'content_type', 'object_id')
+
+
+# List Models
+class List(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField()
+    public = models.BooleanField(default=False)
+
+class ListItem(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    value = models.CharField()
+
+
+# Tracker Models
+class Tracker(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    public = models.BooleanField(default=False)
+
+class TrackerField(models.Model):
+    tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    datatype = models.CharField(max_length=30)
+
+class TrackerItem(models.Model):
+    tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE)
+
+class TrackerItemValue(models.Model):
+    item = models.ForeignKey(TrackerItem, on_delete=models.CASCADE)
+    field = models.ForeignKey(TrackerField, on_delete=models.CASCADE)
+    value = models.CharField()
--- listhive_project/listhive_django/settings.py
+++ listhive_project/listhive_django/settings.py
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL='listhive.CustomUser'
+
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
