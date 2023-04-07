725fe329506d0ce3312b2f64f328767b9114192b
meixingc <realmeixing@gmail.com>
2023-04-06 23:46:17 -0400

    register models

--- listhive_project/listhive/admin.py
+++ listhive_project/listhive/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Follower, Favorite, Like, List, ListItem, Tracker, TrackerField, TrackerItem, TrackerItemValue
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Follower)
+admin.site.register(Favorite)
+admin.site.register(Like)
+admin.site.register(List)
+admin.site.register(ListItem)
+admin.site.register(Tracker)
+admin.site.register(TrackerField)
+admin.site.register(TrackerItem)
+admin.site.register(TrackerItemValue)
