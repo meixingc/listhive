54fd4f829158e5fa461d2a7f014d5f5a8887f4b0
meixingc <realmeixing@gmail.com>
2023-04-07 00:40:52 -0400

    url file cleanup + settings.py added site url

--- listhive_project/listhive_django/settings.py
+++ listhive_project/listhive_django/settings.py
@@ -129,7 +129,6 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL='listhive.CustomUser'
 
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -141,11 +140,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+SITE_URL = 'http://localhost:3000'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
