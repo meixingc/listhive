2a5b14d6c59739a1c655bc920543658566eb0916
meixingc <realmeixing@gmail.com>
2023-04-07 22:39:03 -0400

    nav component completed + nav css

--- listhive_project/listhive_react/src/components/Nav.js
+++ listhive_project/listhive_react/src/components/Nav.js
@@ -1,7 +1,44 @@
+import '../styles/Nav.css'
+import { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
+import { useNavigate, Link } from 'react-router-dom'
+
 export default function Nav() {
-    return (
-        <div>
-            <h1> Nav </h1>
-        </div>
-    )
+    const navigate = useNavigate()
+    const { loggedIn, setLoggedIn } = useContext(UserContext)
+
+    const handleSignout = () => {
+        setLoggedIn(false)
+        navigate('/')
+    }
+
+    if (loggedIn == false) {
+        return (
+            <div className='Nav'>
+                <div>
+                    <img src='/assets/logo-no-background.png' className='nav-logo'/>
+                </div>
+                <div>
+                    <button className='nav-login' onClick={() => navigate('/login')}> Log In or Create Account </button>
+                </div>
+            </div>
+        )
+    } 
+    else {
+        return (
+            <div className='Nav'>
+                <div>
+                    <img src='/assets/logo-no-background.png' className='nav-logo'/>
+                </div>
+                <div className='nav-right'>
+                    <button className='nav-create' onClick={() => navigate('/create')}> Create </button>
+                    <Link to ='/' className='nav-item' back> Home </Link>
+                    <Link to ='/explore' className='nav-item'> Explore </Link>
+                    <Link to ='/profile' className='nav-item'> Profile </Link>
+                    <Link to ='/' onClick={() => handleSignout()} className='nav-item'> Log Out </Link>
+                </div>
+            </div>
+        )
+    }
+
 }
