2de31a4861878499215cef04e43eddb97c3e51cd
meixingc <realmeixing@gmail.com>
2023-04-11 01:06:45 -0400

    remove page context + add pagetitle to main

--- listhive_project/listhive_react/src/components/Main.js
+++ listhive_project/listhive_react/src/components/Main.js
@@ -1,7 +1,12 @@
+import { Route } from "react-router-dom"
+import About from "./main/basic/About"
+import PageTitle from "./PageTitle"
+
 export default function Main() {
     return (
         <div>
-            <h1> Main </h1>
+            <PageTitle />
+            <About />
         </div>
     )
 }
