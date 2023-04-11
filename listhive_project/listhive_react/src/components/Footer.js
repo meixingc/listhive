2ec00b3394d888cdf727817c809b1761862179b5
meixingc <realmeixing@gmail.com>
2023-04-11 01:49:48 -0400

    add portfolio to footer + dividers

--- listhive_project/listhive_react/src/components/Footer.js
+++ listhive_project/listhive_react/src/components/Footer.js
@@ -6,7 +6,11 @@ export default function Footer() {
         <div className='Footer'>
             <div className='footer-text-ctn'>
                 <Link to='/about' className='footer-text'> About Me </Link>
+                <p className='footer-text'> | </p>
+                <a href='https://www.meixingc.com' target='_blank' className='footer-text'> Portfolio </a>
+                <p className='footer-text'> | </p>
                 <a href='https://www.linkedin.com/in/meixingc/' target='_blank' className='footer-text'> LinkedIn </a>
+                <p className='footer-text'> | </p>
                 <a href='https://github.com/meixingc' target='_blank' className='footer-text'> GitHub </a>
             </div>
         </div>
