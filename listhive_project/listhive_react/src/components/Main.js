35a678ebe61648f26b7df7dfbe90be9398332d30
meixingc <realmeixing@gmail.com>
2023-04-11 01:43:05 -0400

    add routes in main + profile edit

--- listhive_project/listhive_react/src/components/Main.js
+++ listhive_project/listhive_react/src/components/Main.js
@@ -1,12 +1,24 @@
-import { Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import About from "./main/basic/About"
 import PageTitle from "./PageTitle"
+import Home from "./main/basic/Home"
+import Login from "./main/users/Login"
+import Explore from "./main/basic/Explore"
+import Profile from "./main/users/Profile"
+import Create from "./main/create/CreateList"
 
 export default function Main() {
     return (
         <div>
             <PageTitle />
-            <About />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/explore' element={<Explore />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/create' element={<Create />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
         </div>
     )
 }
