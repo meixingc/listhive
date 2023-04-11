756103b304a35f96b1fc001df01ccb897612fbeb
meixingc <realmeixing@gmail.com>
2023-04-11 01:36:06 -0400

    footer

--- listhive_project/listhive_react/src/components/Footer.js
+++ listhive_project/listhive_react/src/components/Footer.js
@@ -1,7 +1,14 @@
+import '../styles/Footer.css'
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
     return (
-        <div>
-            <h1> Footer </h1>
+        <div className='Footer'>
+            <div className='footer-text-ctn'>
+                <Link to='/about' className='footer-text'> About Me </Link>
+                <a href='https://www.linkedin.com/in/meixingc/' target='_blank' className='footer-text'> LinkedIn </a>
+                <a href='https://github.com/meixingc' target='_blank' className='footer-text'> GitHub </a>
+            </div>
         </div>
     )
 }
