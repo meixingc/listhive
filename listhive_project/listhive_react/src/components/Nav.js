6d82f19b85bb45196c9204fd3ab488dd3b00e5ac
meixingc <realmeixing@gmail.com>
2023-04-11 01:01:08 -0400

    nav css changes + add home link to nav

--- listhive_project/listhive_react/src/components/Nav.js
+++ listhive_project/listhive_react/src/components/Nav.js
@@ -1,31 +1,22 @@
 import '../styles/Nav.css'
 import { useContext } from 'react'
 import { UserContext } from '../context/UserContext'
-import { PageContext } from '../context/PageContext'
 import { useNavigate, Link } from 'react-router-dom'
 
 export default function Nav() {
     const navigate = useNavigate()
     const { loggedIn, setLoggedIn } = useContext(UserContext)
-    const { setPage } = useContext(PageContext)
 
-    const loginCreate = () => {
-        navigate('/login')
-        setPage('login')
-    }
-    const handleSignout = () => {
-        setLoggedIn(false)
-        navigate('/')
-    }
 
-    if (loggedIn == false) {
+    if (!loggedIn) {
         return (
             <div className='Nav'>
                 <div>
                     <img src='/assets/logo-no-background.png' className='nav-logo'/>
                 </div>
-                <div>
-                    <button className='nav-login' onClick={() => loginCreate()}> Log In </button>
+                <div className='nav-right'>
+                    <Link to ='/' className='nav-item'> Home </Link>
+                    <button className='nav-button' onClick={() => navigate('/login')}> Log In </button>
                 </div>
             </div>
         )
@@ -37,14 +28,13 @@ export default function Nav() {
                     <img src='/assets/logo-no-background.png' className='nav-logo'/>
                 </div>
                 <div className='nav-right'>
-                    <button className='nav-create' onClick={() => navigate('/create')}> Create </button>
-                    <Link to ='/' className='nav-item' back> Home </Link>
+                    <button className='nav-button' onClick={() => navigate('/create')}> Create </button>
+                    <Link to ='/' className='nav-item'> Home </Link>
                     <Link to ='/explore' className='nav-item'> Explore </Link>
                     <Link to ='/profile' className='nav-item'> Profile </Link>
-                    <Link to ='/' onClick={() => handleSignout()} className='nav-item'> Log Out </Link>
+                    <Link to ='/' onClick={() => setLoggedIn(false)} className='nav-item'> Log Out </Link>
                 </div>
             </div>
         )
     }
-
 }
