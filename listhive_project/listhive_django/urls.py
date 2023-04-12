b963d9505810923cf84712c18a4e1c8b08ce2981
meixingc <realmeixing@gmail.com>
2023-04-12 19:11:42 -0400

    remove urls

--- listhive_project/listhive_django/urls.py
+++ listhive_project/listhive_django/urls.py
@@ -3,7 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('listhive.urls')),
 ]
