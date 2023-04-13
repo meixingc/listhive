a36034c7de4bd5b0e1f394d63addb60cc4d6cf22
meixingc <realmeixing@gmail.com>
2023-04-12 21:42:01 -0400

    api rename

--- listhive_project/listhive_react/src/services/api.js
+++ listhive_project/listhive_react/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 
-export const BASE_URL = "http://localhost:8000/api"
+export const BASE_URL = "http://127.0.0.1:8000/api"
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
