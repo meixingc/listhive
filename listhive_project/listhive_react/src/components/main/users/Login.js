463b9d9345d31abc16ce0054a244e1b94cde44da
meixingc <realmeixing@gmail.com>
2023-04-13 00:52:06 -0400

    remove setlogin

--- listhive_project/listhive_react/src/components/main/users/Login.js
+++ listhive_project/listhive_react/src/components/main/users/Login.js
@@ -37,7 +37,6 @@ export default function Login() {
         const sessionData = CheckSession(setUser);
         if (sessionData) {
             setUser(sessionData.user)
-            setLoggedIn(true)
         }
     }, [])
 
