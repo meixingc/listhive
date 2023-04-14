c98dd7999628a4f90bf861c13f010751d9d27a81
meixingc <realmeixing@gmail.com>
2023-04-14 01:40:22 -0400

    add restframework settings.py

--- listhive_project/listhive_django/settings.py
+++ listhive_project/listhive_django/settings.py
@@ -20,6 +20,12 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
